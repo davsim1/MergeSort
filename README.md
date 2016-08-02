0e7471d8c78122e962b69dcc30cf92fdb54a2467
David Simmons <davemsimmons80@gmail.com>
2016-08-02 12:21:16 -0400

    Added Heap Sort

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# MergeSort
-I made this to practice writing sorting algorithms from my own memory.  I used eclipse to debug, but I did not look up how the algorithms work.  Includes selection sort, mergesort, and quicksort.
+# SortingPractice
+I made this to practice writing sorting algorithms in Java from my own memory.  I used Eclipse to debug, but I did not look up how the algorithms work.  Includes selection sort, merge sort, quick sort, and heap sort.
--- sorter/src/sorter/Sorter.java
+++ sorter/src/sorter/Sorter.java
@@ -1,19 +1,20 @@
-// David Simmons 7/28/2016
-// Made to practice making sorting algorithms from my own memory.
+// David Simmons 8/2/2016
+// This was made to practice recreating sorting algorithms from my own memory.
 
 package sorter;
 
 public class Sorter {
 
 	public static void main(String[] args) {
-		int[] list = {3,1,4,1,5,9,2,6,5,3,5,8,9,7,9,3,2,3};
+		int[] list = resetList();
 		System.out.println("Original: ");
 		for(int i : list) {
 			System.out.print(i + ",");
 		}	
 		System.out.println();
+		System.out.println();
 		
-		list = new int[] {3,1,4,1,5,9,2,6,5,3,5,8,9,7,9,3,2,3};
+		list = resetList();
 		selectionSort(list);
 		System.out.println("Selection sort: ");
 		for(int i : list) {
@@ -21,29 +22,41 @@ public class Sorter {
 		}	
 		System.out.println();
 		
-		list = new int[] {3,1,4,1,5,9,2,6,5,3,5,8,9,7,9,3,2,3};
+		list = resetList();
 		quicksort(list);
-		System.out.println("Quicksort: ");
+		System.out.println("Quick sort: ");
 		for(int i : list) {
 			System.out.print(i + ",");
 		}	
 		System.out.println();
 		
-		list = new int[] {3,1,4,1,5,9,2,6,5,3,5,8,9,7,9,3,2,3};
+		list = resetList();
 		list = mergesort(list);
-		System.out.println("Mergesort: ");
+		System.out.println("Merge sort: ");
 		for(int i : list) {
 			System.out.print(i + ",");
 		}	
 		System.out.println();
 		
-		list = new int[] {3,1,4,1,5,9,2,6,5,3,5,8,9,7,9,3,2,3};
+		list = resetList();
 		System.out.println("Tree Sort: ");
 		treeSortPrint(list);
+		System.out.println();
 		
+		list = resetList();
+		list = heapsort(list);
+		System.out.println("Heap sort: ");
+		for(int i : list) {
+			System.out.print(i + ",");
+		}	
+		System.out.println();
+	}
+	
+	public static int[] resetList() {
+		return new int[] {3,1,4,1,5,9,2,6,5,3,5,8,9,7,9,3,2,3};
 	}
 
-	// Selection
+	// ----------- Selection Sort ---------------------------------------------
 	public static void selectionSort(int[] input) {
 		int minIndex = 0;
 		int temp = -1;
@@ -62,11 +75,13 @@ public class Sorter {
 		
 	}
 	
-	// Quick
+	// ----------- Quick Sort ---------------------------------------------
 	public static void quicksort(int[] list) {
 		quicksort(list, 0, list.length -1);
 	}
 
+	// Quick sort list between i and j.  This could use a random index instead
+	// of j to help prevent the O(n^2) behavior.
 	private static void quicksort(int[] list, int i, int j) {
 		if (j < list.length && (j - i) >= 1) {
 			int border = i;
@@ -88,7 +103,7 @@ public class Sorter {
 		}
 	}
 
-	// Merge
+	// ----------- Merge Sort ---------------------------------------------
 	public static int[] mergesort(int[] list) {
 		int[] result = new int[list.length];
 		int i = 0;
@@ -108,7 +123,8 @@ public class Sorter {
 	private static int[] split(int[] result, int i, int j) {
 		if (j - i > 2) {
 			// return the combination of each sorted half between i and j
-			return combine(split(result, i, i + ((j - i) / 2) + 1), split(result, i + ((j - i) / 2) + 1, j));
+			return combine(split(result, i, i + ((j - i) / 2) + 1), 
+					split(result, i + ((j - i) / 2) + 1, j));
 		} else {
 			int[] res;
 			// If there is one element left, return it
@@ -182,7 +198,7 @@ public class Sorter {
 		return result;
 	}
 	
-	// Tree 
+	// ----------- Tree Sort ---------------------------------------------
 	public static class Node {
 		int data;
 		Node left;
@@ -193,6 +209,8 @@ public class Sorter {
 		}
 	}
 
+	// Create a binary search tree from the list and do an in order traversal
+	// to print the elements in ascending order
 	public static void treeSortPrint(int[] list) {
 		Node tree = null;
 
@@ -221,5 +239,120 @@ public class Sorter {
 			System.out.print(tree.data + ",");
 			printInOrder(tree.right);
 		}
+	}
+	
+	// ----------- Heap Sort ---------------------------------------------
+	public static int[] heapsort(int[] list) {
+		// Make max heap
+		int[] result = heapify(list);
+		
+		// Get each element in descending order with side effect of 
+		// sorting the list ascending
+		for (int i = list.length - 1; i >= 0; i--) {
+			get(result, i);
+		}
+		
+		return result;
+	}
+
+	// Make a new heap and add all of the inputs to it
+	public static int[] heapify(int[] list) {
+		int[] result = new int[list.length];
+		
+		for(int i = 0; i < list.length; i++) {
+			add(result, i, list[i]);
+		}
+
+		return result;
+	}
+
+	// Add and element to the list as if it were a heap by putting the new
+	// element at the bottom and bubbling it up
+	public static void add(int[] list, int i, int num) {
+		int parent = 0;	
+		list[i] = num;
+		
+		if (i != 0) {
+			parent = getParent(list, i);
+			while(i > 0 && list[parent] < num) {
+				swap(list, parent, i);
+				i = parent;
+				if (i != 0) {
+					parent = getParent(list, i);
+				}
+			}
+		}
+	}
+
+	// Get the max element from list as if it were a max heap then 
+	// restore the heap by swapping a bottom element to the top
+	// and bubbling it down
+	public static int get(int[] list, int i) {
+		int result = list[0];
+		int num = -1;
+		int max = -1;
+		int j = 0;
+		boolean swapMade = true;
+
+		if (i > 0) {
+			swap(list, 0, i);
+			num = list[0];
+			j = 0;
+
+			while (swapMade == true && j < i) {
+				swapMade = false;
+				if (hasLeft(i, j) && hasRight(i, j)) {
+					max = maxIndex(list, getLeft(j), getRight(j));		
+				} else if (hasLeft(i, j)) {
+					max = getLeft(j);
+				} else {
+					max = j;
+				}
+		
+				if (max != j && list[max] > list[j]) {
+					swap(list, max, j);
+					swapMade = true;
+					j = max;
+				}
+			}
+		}
+		
+		return result;
+	}
+
+	// Return the index i or j corresponding to the max element in list
+	public static int maxIndex(int[] list, int i, int j) {
+		return (list[i] >= list[j])? i : j;
+	}
+
+	// Return if there is a left child of the element at index i within the bound
+	public static boolean hasLeft(int bound, int i) {
+		return (i * 2 + 1) >= 0 && (i * 2 + 1) < bound;
+	}
+
+	// Return if there is a right child of the element at index i within the bound
+	public static boolean hasRight(int bound, int i) {
+		return (i * 2 + 2) >= 0 && (i * 2 + 2) < bound;
+	}
+
+	// Return the index of the left child of the element at index i
+	public static int getLeft(int i) {
+		return i * 2 + 1;
+	}
+
+	// Return the index of the right child of the element at index i
+	public static int getRight(int i) {
+		return i * 2 + 2;
+	}
+
+	public static void swap(int[] list, int i, int j) {
+		int temp = list[i];
+		list[i] = list[j];
+		list[j] = temp;
+	}
+
+	// Return the index of the parent of the element at index i in list
+	public static int getParent(int[] list, int i) {
+		return (i-1)/2;
 	}
 }
